5a3bd0f9119874915d1c37e199b9f1d044e45f83
David Grudl <david@grudl.com>
2022-01-02 17:04:26 +0100

    tests: must use shared temp directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,6 +17,5 @@ date_default_timezone_set('Europe/Prague');
 
 
 // create temporary directory
-define('TEMP_DIR', __DIR__ . '/tmp/' . lcg_value());
-@mkdir(dirname(TEMP_DIR));
+define('TEMP_DIR', __DIR__ . '/tmp');
 @mkdir(TEMP_DIR);
